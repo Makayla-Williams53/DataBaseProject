5d489402bd38dfeee097d2ab4fe9232eca77cf56
Makayla Williams <90784764+Makayla-Williams53@users.noreply.github.com>
2023-01-24 18:57:04 +0000

    Added the 3rd and 4th internships to the table

--- main.sql
+++ main.sql
@@ -10,7 +10,11 @@ CREATE TABLE internships
 
 INSERT INTO internships values
 ('Aeronautical Engineer(Plasma)', 'Lockheed Martin', 'Palmdale, California', 40, true, '2022/12/20'),
-('Software Engineer (Signature Management)', 'Lockheed Martin', 'Palmdale, California', 40, true, '2022/12/06');
+('Software Engineer (Signature Management)', 'Lockheed Martin', 'Palmdale, California', 40, true, '2022/12/06')
+('Software Engineer Intern', 'Raytheon Tecnologies', 'Schriever Air Force Base, Colorado', 30, false, '2023/01/13'),
+('Aerospace Engineering Intern', 'General Atomics Aeronautical Systems', 'Poway, California', 40, true, '2023/01/09'),
+('Aerostructures Design Engineering Intern', 'General Atomics Aeronautical Systems', 'Poway, California', 40, true, '2022/01/05'),
+('2023 Propulsion Engineer Undergraduate Intern', 'Aerospace Corporation', 'El Segundo, California', 40, false, '2023/01/23'),
 
 
 .print
