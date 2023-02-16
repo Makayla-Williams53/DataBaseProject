169c84cfb7d63020ea0095575a21945e12abbe6c
Makayla Williams <90784764+Makayla-Williams53@users.noreply.github.com>
2023-02-16 20:26:23 +0000

    Have changed the queries so that they are actually queries

--- main.sql
+++ main.sql
@@ -5,40 +5,53 @@ CREATE TABLE internships
   location TEXT,
   num_hours INTEGER,
   is_paid boolean,
+  clearance TEXT,
   posted DATE
 );
 
 INSERT INTO internships values
-('Aeronautical Engineer(Plasma)', 'Lockheed Martin', 'Palmdale, California', 40, true, '2022/12/20'),
-('Software Engineer (Signature Management)', 'Lockheed Martin', 'Palmdale, California', 40, true, '2022/12/06'),
-('Software Engineer Intern', 'Raytheon Tecnologies', 'Schriever Air Force Base, Colorado', 30, false, '2023/01/13'),
-('Aerospace Engineering Intern', 'General Atomics Aeronautical Systems', 'Poway, California', 40, true, '2023/01/09'),
-('Aerostructures Design Engineering Intern', 'General Atomics Aeronautical Systems', 'Poway, California', 40, true, '2022/01/05'),
-('2023 Propulsion Engineer Undergraduate Intern', 'Aerospace Corporation', 'El Segundo, California', 40, false, '2023/01/23'),
-('Engineering Intern', 'Raytheon Technologies', 'Winston-Salem, North Carolina', 30, false, '2022/08/22'),
-('Software Engineer (Summer Intern)', 'Lockheed Martin', 'Sunnyvale, California', 40, true, '2023/01/19'),
-('Aerospace Engineer (Summer Intern)', 'Lockheed Martin', 'Lone Tree, Colorado', 40, true, '2023/01/15'),
-('2023 Software Development Undergraduate Intern', 'Aerospace Corporation', 'El Segundo, California', 40, false, '2023/01/18');
+('Aeronautical Engineer(Plasma)', 'Lockheed Martin', 'Palmdale, California', 40, true, "Secret", '2022/12/20'),
+('Software Engineer (Signature Management)', 'Lockheed Martin', 'Palmdale, California', 40, true, "Secret",  '2022/12/06'),
+('Software Engineer Intern', 'Raytheon Tecnologies', 'Schriever Air Force Base, Colorado', 30, null, "None", '2023/01/13'),
+('Aerospace Engineering Intern', 'General Atomics Aeronautical Systems', 'Poway, California', 40, true, "None", '2023/01/09'),
+('Aerostructures Design Engineering Intern', 'General Atomics Aeronautical Systems', 'Poway, California', 40, true, "None", '2022/01/05'),
+('2023 Propulsion Engineer Undergraduate Intern', 'Aerospace Corporation', 'El Segundo, California', 40, null, "None", '2023/01/23'),
+('Engineering Intern', 'Raytheon Technologies', 'Winston-Salem, North Carolina', 30, null, "None", '2022/08/22'),
+('Software Engineer (Summer Intern)', 'Lockheed Martin', 'Sunnyvale, California', 40, true, "Secret", '2023/01/19'),
+('Aerospace Engineer (Summer Intern)', 'Lockheed Martin', 'Lone Tree, Colorado', 40, true, "Secret", '2023/01/15'),
+('2023 Software Development Undergraduate Intern', 'Aerospace Corporation', 'El Segundo, California', 40, null, "None", '2023/01/18');
 
 --sort by company location name alphabetically
+.print
+.print "Table sorted alphabetically by company location:"
 .print
 SELECT 
 * FROM internships
 ORDER BY location;
 .print
   
- --sort by is_paid by false to true
+ --sort by date posted
+.print
+.print "Table sorted by posted date oldest to newest:"
 .print
 SELECT * FROM internships
-ORDER BY is_paid;
+ORDER BY posted;
 .print
 
---query by position name
+--query of jobs that the pay is unknown
 .print
-SELECT position FROM internships;
+.print "Table query for job where the pay is unknown:"
+.print
+SELECT "Job payment is unknown: " AS ColumnName, is_paid AS ColumnName,
+* FROM internships
+WHERE is_paid IS NULL;
 .print
 
---query by company name 
+--query of jobs 
 .print
-SELECT company FROM internships;
+.print "Table query for job where the clearance level is secret:"
+.print
+SELECT "Clearance level is scret: " AS ColumnName, clearance As ColumnName,
+* FROM  internships
+WHERE clearance = 'Secret';
 .print
